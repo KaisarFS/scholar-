8098b4bc011fdf786777a30acc3aab1f14d02052
Kaisar Firjatullah Sani Setyawan <kaisar.10rpl2.2019@gmail.com>
2024-09-01 20:45:14 +0700

    add comment for routes query and params

--- routes/mahasiswa.js
+++ routes/mahasiswa.js
@@ -6,6 +6,7 @@ router
   .get('/lists', lists)
   .post('/stores', stores)
   .get('/info', info)
-  .delete('/:id', deleteMahasiswa)
+  .delete('/:id', deleteMahasiswa) // disini saya sengaja menggunakan /:id (params), ketimbang query seperti routes /info. hanya utk contoh / metode lain
+
 
 module.exports = router;
